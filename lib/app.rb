0278bd516c943143c1d9ab9ccd1a380fc3e1202b
realsimon <realsimon@gmail.com>
2016-02-08 19:06:49 -0500

    Some more comments and first half of brands

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,5 @@
 require 'json'
+
 #replaced with "setup_files" method:
 #path = File.join(File.dirname(__FILE__), '../data/products.json')
 #file = File.read(path)
@@ -8,14 +9,16 @@ require 'json'
 $indent = "   "
 $report_file = $stderr
 
+# Set up input/output files:
 def setup_files
     path = File.join(File.dirname(__FILE__), '../data/products.json')
     file = File.read(path)
     $products_hash = JSON.parse(file)
+    # Comment out next line for screen output:
     #$report_file = File.new("report.txt", "w+")
 end
 
-#Use command line "figlet" to print ascii art banner to a file or stderr
+# Use command line "figlet" utility to print ascii art banner:
 def print_banner(banner_text)
   banner = `figlet "#{$indent}#{banner_text}"`
   $report_file.puts
@@ -23,6 +26,7 @@ def print_banner(banner_text)
   $report_file.puts
 end
 
+# Calculate total:
 def do_total(hash, field)
   total = 0.0
   total = hash.inject(total) {|total, element | total + element[field].to_f}
@@ -30,7 +34,7 @@ def do_total(hash, field)
 end
 
 
-
+# Print "products" part of sales report:
 def products_report
 
   # Print "Products" in ascii art
@@ -56,24 +60,46 @@ def products_report
 
 	# Calculate and print the average price the toy sold for
   average_sale = sales_sum / sales_num
-  puts "Average sale price: #{average_sale}"
+  $report_file.puts "Average sale price: #{average_sale}"
 
 	# Calculate and print the average discount (% or $) based off the average sales price
-  puts "Average discount: #{((1 - (average_sale / toy["full-price"].to_f)) * 100).round(2)} %"
+  $report_file.puts "Average discount: #{((1 - (average_sale / toy["full-price"].to_f)) * 100).round(2)} %"
 
-  puts
+  $report_file.puts
   end
 end
 
-# Print "Brands" in ascii art
-print_banner "Brands"
+def brands_report
 
-# For each brand in the data set:
-	# Print the name of the brand
-	# Count and print the number of the brand's toys we stock
-	# Calculate and print the average price of the brand's toys
-	# Calculate and print the total sales volume of all the brand's toys combined
+  # Print "Brands" in ascii art
+  print_banner "Brands"
 
+  # Get all brands:
+  brands = ($products_hash["items"].map {|toy| toy["brand"]}).uniq
+
+  # For each brand in the data set:
+  brands.each do |brand|
+
+    # Print the name of the brand
+    $report_file.puts "Brand: \"#{brand}\""
+
+    # Select items for each brand
+    brand_toys = $products_hash["items"].select {|toy| toy["brand"] == brand}
+
+    # Count and print the number of the brand's toys we stock
+    $report_file.puts "#{brand} inventory: #{do_total(brand_toys, "stock")}"
+
+    # Calculate and print the average price of the brand's toys
+
+
+	  # Calculate and print the total sales volume of all the brand's toys combined
+
+
+  $report_file.puts
+  end
+end
+
+# Start report execution here:
 
 setup_files
 
@@ -84,3 +110,5 @@ print_banner "Sales Report"
 $report_file.puts "#{$indent}#{Time.new}"
 
 products_report
+
+brands_report
